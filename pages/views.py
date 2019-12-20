c94cf3df01ad2324b2b869b5e337011cd6152ec1
Suvendu Kumar Behera <Suvendu@Apples-MacBook-Pro.local>
2019-12-20 13:24:42 +0530

    changed ip

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def health_check_view(request):
-    response = requests.get("http://172.34.0.149:9000/api/health/")
+    response = requests.get("http://172.31.27.81:9000/api/health/")
     return HttpResponse('Hello, World! <br><br>From API Server: ' + str(response.json()['timestamp']))
